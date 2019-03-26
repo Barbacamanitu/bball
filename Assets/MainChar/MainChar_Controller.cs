55225edf5156323c6839d2c0baf2a569fb05ab9b
Barbacamanitu <russmbiz@gmail.com>
2019-03-25 22:26:08 -0500

    a
    s

--- Assets/MainChar/MainChar_Controller.cs
+++ Assets/MainChar/MainChar_Controller.cs
@@ -17,22 +17,6 @@ public class MainChar_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 lStick = new Vector2(Input.GetAxis("LStickX"), Input.GetAxis("LStickY"));
-        if (lStick.magnitude > 0.0f)
-        {
-            Vector3 movement = transform.forward * Speed * lStick.y;
-            Vector3 strafe = transform.right * Strafe * lStick.x;
 
-          
-            cc.SimpleMove(movement + strafe);
-
-        }
-        Vector2 rStick = new Vector2(Input.GetAxis("RStickX"), Input.GetAxis("RStickY"));
-        if (rStick.magnitude > 0.0f)
-        {
-            transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), rStick.x * RotateSpeed);
-        }
-
-       
     }
 }
